57b910970e55ca6e2f7d214bdfef905f411ada97
1970Mr <mrepoch1970@gmail.com>
2024-06-26 14:30:51 +0430

    Create TransactionService

--- app/Http/Controllers/MembershipPlanController.php
+++ app/Http/Controllers/MembershipPlanController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\Status;
 use App\Models\MembershipPlan;
-use App\Models\Purchase;
-use App\Models\Transaction;
+use App\Services\TransactionService;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use Shetabit\Multipay\Exceptions\InvalidPaymentException;
 use Shetabit\Multipay\Exceptions\InvoiceNotFoundException;
 use Shetabit\Multipay\Invoice;
-use Shetabit\Payment\Facade\Payment;
 
 class MembershipPlanController extends Controller
 {
+    public function __construct(private readonly TransactionService $transactionService)
+    {
+    }
+
     public function index()
     {
         $plans = MembershipPlan::all();
@@ -27,27 +29,11 @@ class MembershipPlanController extends Controller
         try {
             $membershipPlan = MembershipPlan::query()->findOrFail($request->plan_id);
             $invoice = (new Invoice())->amount($membershipPlan->price);
-            $transaction = Transaction::query()->create([
-                'payment_id' => uniqid('', true),
-                'amount' => $invoice->getAmount(),
-                'product_type' => get_class($membershipPlan),
-                'product_id' => $membershipPlan->id,
-                'invoice_details' => $invoice,
-                'status' => Status::Pending,
-                'user_id' => Auth::id(),
-            ]);
-            Payment::callbackUrl(route('membership-plan.callback'));
+            $callbackUrl = route('membership-plan.callback');
 
-            $payment = Payment::purchase($invoice, static function ($driver, $transactionId) use ($transaction) {
-                $transaction->update([
-                    'transaction_id' => $transactionId,
-                ]);
-            });
-
-            return $payment->pay()->render();
-        } catch (\Exception $e) {
-            logger($e->getMessage());
-            return back()->withErrors(['error' => $e->getMessage()]);
+            return $this->transactionService->purchase($invoice, $membershipPlan, $callbackUrl);
+        } catch (Exception $e) {
+            return back()->withErrors(['error' => 'Failed to purchase!']);
         }
     }
 
@@ -55,30 +41,15 @@ class MembershipPlanController extends Controller
     {
         try {
             $transaction_id = $request->get('Authority');
-            $transaction = Transaction::query()->where('transaction_id', $transaction_id)->first();
-            $receipt = Payment::amount($transaction?->amount)->transactionId($transaction_id)->verify();
-            $transaction->update([
-                'status' => Status::Success,
-                'transaction_result' => $receipt,
-                'reference_id' => $receipt->getReferenceId(),
-            ]);
-            Purchase::query()->create([
-                'user_id' => Auth::id(),
-                'product_type' => $transaction->product_type,
-                'product_id' => $transaction->product_id,
-            ]);
-            Auth::user()->update([
-                'membership_plan_id' => $transaction->product_id,
-                'membership_expires_at' => now()->addDays($transaction->product->duration),
-            ]);
+            $transaction_id = $this->transactionService->callback($transaction_id, static function ($transaction) {
+                Auth::user()->update([
+                    'membership_plan_id' => $transaction->product_id,
+                    'membership_expires_at' => now()->addDays($transaction->product->duration),
+                ]);
+            });
+
             return view('transactions.callback', ['status' => 'success', 'transaction_id' => $transaction_id]);
         } catch (InvalidPaymentException|InvoiceNotFoundException $e) {
-            logger($e->getMessage());
-            $transaction_id = $request->get('Authority');
-            $transaction = Transaction::query()->where('transaction_id', $transaction_id)->first();
-            $transaction?->update([
-                'status' => Status::Failed,
-            ]);
             return view('transactions.callback', ['status' => 'error']);
         }
     }
---
+++ app/Services/TransactionService.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\Status;
+use App\Models\Purchase;
+use App\Models\Transaction;
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Shetabit\Multipay\Exceptions\InvalidPaymentException;
+use Shetabit\Multipay\Exceptions\InvoiceNotFoundException;
+use Shetabit\Multipay\Invoice;
+use Shetabit\Payment\Facade\Payment;
+
+class TransactionService
+{
+    /**
+     * @throws Exception
+     */
+    public function purchase(Invoice $invoice, Model $model, string $callbackUrl): mixed
+    {
+        try {
+            $transaction = $this->createTransaction($invoice, $model);
+            return $this->processPayment($invoice, $callbackUrl, $transaction);
+        } catch (Exception $e) {
+            logger($e->getMessage());
+            throw $e;
+        }
+    }
+
+    /**
+     * @throws InvoiceNotFoundException
+     * @throws InvalidPaymentException
+     */
+    public function callback(string $transactionId, ?callable $callbackFunc = null): int
+    {
+        try {
+            $transaction = $this->getTransactionById($transactionId);
+            $receipt = $this->verifyPayment($transaction);
+            $this->updateTransactionSuccess($transaction, $receipt);
+            $this->createPurchase($transaction);
+
+            if ($callbackFunc) {
+                $callbackFunc($transaction);
+            }
+
+            return $transactionId;
+        } catch (InvalidPaymentException|InvoiceNotFoundException $e) {
+            logger($e->getMessage());
+            $this->updateTransactionFailure($transactionId);
+            throw $e;
+        }
+    }
+
+    private function createTransaction(Invoice $invoice, Model $model): Transaction
+    {
+        return Transaction::query()->create([
+            'payment_id' => uniqid('', true),
+            'amount' => $invoice->getAmount(),
+            'product_type' => get_class($model),
+            'product_id' => $model->id,
+            'invoice_details' => $invoice,
+            'status' => Status::Pending,
+            'user_id' => Auth::id(),
+        ]);
+    }
+
+    private function processPayment(Invoice $invoice, string $callbackUrl, Transaction $transaction): mixed
+    {
+        Payment::callbackUrl($callbackUrl);
+        $payment = Payment::purchase($invoice, static function ($driver, $transactionId) use ($transaction) {
+            $transaction->update(['transaction_id' => $transactionId]);
+        });
+
+        return $payment->pay()->render();
+    }
+
+    private function getTransactionById(string $transactionId): Transaction
+    {
+        $transaction = Transaction::query()
+            ->where('transaction_id', $transactionId)
+            ->where('user_id', Auth::id())
+            ->first();
+        abort_if(is_null($transaction), 404, 'Transaction not found');
+        return $transaction;
+    }
+
+    private function verifyPayment(Transaction $transaction): mixed
+    {
+        return Payment::amount($transaction->amount)
+            ->transactionId($transaction->transaction_id)
+            ->verify();
+    }
+
+    private function updateTransactionSuccess(Transaction $transaction, $receipt): void
+    {
+        $transaction->update([
+            'status' => Status::Success,
+            'transaction_result' => $receipt,
+            'reference_id' => $receipt->getReferenceId(),
+        ]);
+    }
+
+    private function createPurchase(Transaction $transaction): void
+    {
+        Purchase::query()->create([
+            'user_id' => Auth::id(),
+            'product_type' => $transaction->product_type,
+            'product_id' => $transaction->product_id,
+        ]);
+    }
+
+    private function updateTransactionFailure(string $transactionId): void
+    {
+        $transaction = $this->getTransactionById($transactionId);
+        $transaction?->update(['status' => Status::Failed]);
+    }
+}
