d8bec1dbf4d806c97017b20d21eeb5ccb2c5125e
1970Mr <mrepoch1970@gmail.com>
2024-07-03 18:17:15 +0430

    Relocation of transaction operations to the TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\PaymentMethod;
+use App\Exceptions\VerifyRepeatedException;
+use App\Http\Requests\OrderRequest;
+use App\Models\Transaction;
+use App\Services\Order\Order as OrderService;
+use App\Services\Transaction\Transaction as TransactionService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+use Illuminate\View\View;
+use Shetabit\Multipay\Exceptions\InvalidPaymentException;
+use Shetabit\Multipay\Exceptions\InvoiceNotFoundException;
+use Shetabit\Multipay\Invoice;
+use Exception;
+
 class TransactionController extends Controller
 {
+    public function __construct(
+        private readonly OrderService $orderService,
+        private readonly TransactionService $transactionService,
+    )
+    {
+    }
 
+    public function checkout(OrderRequest $request): mixed
+    {
+        DB::beginTransaction();
+        try {
+            $order = $this->orderService->makeOrder();
+            $transaction = $this->transactionService->createTransaction(
+                $order,
+                $request->get('payment_method'),
+                $request->get('payment_gateway'),
+            );
+
+            // If the payment is not online
+            if ($request->payment_method !== PaymentMethod::ONLINE->value) {
+                DB::commit();
+                return to_route('home')->with(['success' => __('Your order has been successfully placed.')]);
+            }
+
+            $invoice = (new Invoice())->amount($order->amount);
+            $callbackUrl = route('transactions.callback', $transaction->internal_code);
+
+            DB::commit();
+            return $this->transactionService->processPayment($transaction, $invoice, $callbackUrl);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return back()->with(['error' => __('Payment failed!')]);
+        }
+    }
+
+    public function callback(Transaction $transaction): View
+    {
+        try {
+            // To verify that the transaction belongs to the user
+            $this->transactionService->ensureTransactionBelongsToUser($transaction->transaction_id);
+            $referenceId = $this->transactionService->verify($transaction);
+
+            return $this->setView('success', __('Payment successfully.'), $referenceId);
+        } catch (VerifyRepeatedException $e) {
+            return $this->setView('success', __('Payment already done!'), $transaction->reference_id);
+        } catch (InvalidPaymentException|InvoiceNotFoundException|Exception $e) {
+            return $this->setView('error', __('Payment failed!'));
+        }
+    }
+
+    public function setView(string $status, string $message, ?int $referenceId = null): View
+    {
+        return view('transactions.callback', compact(['status', 'message', 'referenceId']));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\MembershipPlanController;
-use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
-use App\Services\Storage\Contracts\StorageInterface;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +12,6 @@ Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::post('/products/checkout', [ProductController::class, 'checkout'])->name('products.checkout');
 Route::get('/products/callback', [ProductController::class, 'callback'])->name('products.callback');
-
-// Transaction
-Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
 
 // Membership
 Route::get('/membership-plans', [MembershipPlanController::class, 'index'])->name('membership-plans.index');
@@ -28,12 +23,9 @@ Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
 Route::patch('/cart/{product}', [CartController::class, 'update'])->name('cart.update');
 Route::delete('/cart/{product}', [CartController::class, 'delete'])->name('cart.delete');
-// Temporary route
-Route::get('/cart/clear', static function (StorageInterface $storage) {
-    $storage->clear();
-})->name('cart.clear');
 
-// Order
-Route::post('/transactions/checkout', [OrderController::class, 'checkout'])->name('transactions.checkout');
-Route::any('/transactions/callback/{transaction:internal_code}', [OrderController::class, 'callback'])->name('transactions.callback')
+// Transaction
+Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+Route::post('/transactions/checkout', [TransactionController::class, 'checkout'])->name('transactions.checkout');
+Route::any('/transactions/callback/{transaction:internal_code}', [TransactionController::class, 'callback'])->name('transactions.callback')
     ->withoutMiddleware(VerifyCsrfToken::class);
