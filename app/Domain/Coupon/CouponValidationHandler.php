53a9eaef4458b075a75998631f9911946435ff95
1970Mr <mrepoch1970@gmail.com>
2024-07-09 21:09:03 +0430

    Add validation for minimum amount and fixed amount for coupon

--- app/Domain/Coupon/CouponValidationHandler.php
+++ app/Domain/Coupon/CouponValidationHandler.php
@@ -4,6 +4,8 @@ namespace App\Domain\Coupon;
 
 use App\Domain\Coupon\Validator\BelongsToUser;
 use App\Domain\Coupon\Validator\HasUsageLimit;
+use App\Domain\Coupon\Validator\HasValidAmount;
+use App\Domain\Coupon\Validator\HasValidMinimumAmount;
 use App\Domain\Coupon\Validator\HasValidTime;
 use App\Exceptions\InvalidCouponException;
 use App\Models\Coupon;
@@ -22,9 +24,13 @@ class CouponValidationHandler
         $isValidExpiration = resolve(HasValidTime::class);
         $canUseIt = resolve(BelongsToUser::class);
         $hasUsageLimit = resolve(HasUsageLimit::class);
+        $hasValidAmount = resolve(HasValidAmount::class);
+        $hasValidMinimumAmount = resolve(HasValidMinimumAmount::class);
 
         $isValidExpiration->setNextValidator($canUseIt);
         $canUseIt->setNextValidator($hasUsageLimit);
+        $hasUsageLimit->setNextValidator($hasValidAmount);
+        $hasValidAmount->setNextValidator($hasValidMinimumAmount);
 
         return $isValidExpiration->validate($coupon);
     }
---
+++ app/Domain/Coupon/Validator/HasValidFixedAmount.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Domain\Coupon\Validator;
+
+use App\Domain\Cost\Contracts\CostInterface;
+use App\Enums\CouponType;
+use App\Exceptions\InvalidCouponException;
+use App\Models\Coupon;
+
+class HasValidAmount extends BaseCouponValidator
+{
+    public function __construct(readonly private CostInterface $cost)
+    {
+    }
+
+    /**
+     * @throws InvalidCouponException
+     */
+    public function validate(Coupon $coupon): bool
+    {
+        $orderAmount = $this->cost->calculateTotalCost();
+
+        if ($coupon->amount_type === CouponType::FIXED_AMOUNT && $coupon->amount >= $orderAmount) {
+            throw new InvalidCouponException();
+        }
+
+        return parent::validate($coupon);
+    }
+}
