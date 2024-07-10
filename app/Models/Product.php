552fd60f387ac6c2890d6573b85f77c70777a83b
1970Mr <mrepoch1970@gmail.com>
2024-07-10 21:35:33 +0430

    Refactor the product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,7 @@
 
 namespace App\Models;
 
-use App\Services\Discount\AmazingSale\AmazingSaleDiscountCalculator;
-use App\Services\Discount\CommonDiscount\CommonDiscountCalculator;
+use App\Traits\HasDiscount;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -11,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, HasDiscount;
 
     protected $fillable = [
         'name',
@@ -29,53 +28,6 @@ class Product extends Model
     public function hasStock(int $quantity): bool
     {
         return $this->stock >= $quantity;
-    }
-
-    public function discountedPrice(): int
-    {
-        if (!$this->hasDiscount()) {
-            return $this->price;
-        }
-
-        if ($this->hasCommonDiscount()) {
-            return $this->commonDiscountPrice();
-        }
-
-        return $this->amazingSalePrice();
-    }
-
-    public function hasDiscount(): bool
-    {
-        return $this->hasCommonDiscount() || $this->hasAmazingSale();
-    }
-
-    public function commonDiscountPrice(): int
-    {
-        if (!$this->hasCommonDiscount()) {
-            return $this->price;
-        }
-        $commonDiscount = $this->commonDiscount();
-        return (new CommonDiscountCalculator($commonDiscount))->discountedPrice($this->price);
-    }
-
-    public function hasAmazingSale(): bool
-    {
-        return $this->amazingSale()->exists();
-    }
-
-    public function amazingSalePrice(): int
-    {
-        if (!$this->hasAmazingSale()) {
-            return $this->price;
-        }
-        /** @var AmazingSale $amazingSale */
-        $amazingSale = $this->amazingSale()->first();
-        return (new AmazingSaleDiscountCalculator($amazingSale))->discountedPrice($this->price);
-    }
-
-    public function hasCommonDiscount(): bool
-    {
-        return $this->commonDiscount()?->hasValidMinimumAmount($this->price);
     }
 
     public function commonDiscount(): ?CommonDiscount
---
+++ app/Traits/HasDiscount.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\AmazingSale;
+use App\Services\Discount\AmazingSale\AmazingSaleDiscountCalculator;
+use App\Services\Discount\CommonDiscount\CommonDiscountCalculator;
+
+trait HasDiscount
+{
+    public function discountedPrice(): int
+    {
+        if (!$this->hasDiscount()) {
+            return $this->price;
+        }
+
+        if ($this->hasCommonDiscount()) {
+            return $this->commonDiscountPrice();
+        }
+
+        return $this->amazingSalePrice();
+    }
+
+    public function hasDiscount(): bool
+    {
+        return $this->hasCommonDiscount() || $this->hasAmazingSale();
+    }
+
+    public function hasCommonDiscount(): bool
+    {
+        return $this->commonDiscount()?->hasValidMinimumAmount($this->price);
+    }
+
+    public function commonDiscountPrice(): int
+    {
+        if (!$this->hasCommonDiscount()) {
+            return $this->price;
+        }
+        $commonDiscount = $this->commonDiscount();
+        return (new CommonDiscountCalculator($commonDiscount))->discountedPrice($this->price);
+    }
+
+    public function hasAmazingSale(): bool
+    {
+        return $this->amazingSale()->exists();
+    }
+
+    public function amazingSalePrice(): int
+    {
+        if (!$this->hasAmazingSale()) {
+            return $this->price;
+        }
+        /** @var AmazingSale $amazingSale */
+        $amazingSale = $this->amazingSale()->first();
+        return (new AmazingSaleDiscountCalculator($amazingSale))->discountedPrice($this->price);
+    }
+}
